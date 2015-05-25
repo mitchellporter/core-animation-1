ec4ff9be141446fac3f494810bc55ada6811d696
Mitchell Porter <mitchellporter@gmail.com>
2015-05-25 16:17:39 -0700

    Chapter 2. Adding blueLayer, then updating after timer with red circle

--- Core Animation 1/ViewController.m
+++ Core Animation 1/ViewController.m
@@ -11,6 +11,7 @@
 @interface ViewController ()
 
 @property UIView *layerView;
+@property CALayer *blueLayer;
 
 @end
 
@@ -24,19 +25,42 @@
     self.view.backgroundColor = [UIColor grayColor];
     
     // Create sublayer
-    CALayer *blueLayer = [CALayer layer];
-    blueLayer.frame = CGRectMake(50, 50, 100, 100);
-    blueLayer.backgroundColor = [UIColor blueColor].CGColor;
+    self.blueLayer = [CALayer layer];
+    self.blueLayer.frame = CGRectMake(50, 50, 100, 100);
+    self.blueLayer.backgroundColor = [UIColor blueColor].CGColor;
+    
+    // Set controller as layer delegate
+    self.blueLayer.delegate = self;
+    
+    // Ensure that layer backing image uses correct scale
+    self.blueLayer.contentsScale = [UIScreen mainScreen].scale;
+    
     
     self.layerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
-    self.layerView.backgroundColor = [UIColor whiteColor];
     self.layerView.frame = CGRectMake(self.view.frame.size.width/2 - self.layerView.frame.size.width/2, self.view.frame.size.height/2 - self.layerView.frame.size.height, self.layerView.frame.size.width, self.layerView.frame.size.height);
     
-    // Add sublayer to the layer tree
-    [self.layerView.layer addSublayer:blueLayer];
+    // Add layer to our view
+    [self.layerView.layer addSublayer:self.blueLayer];
+    
+    // Redraw layer
+    [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(updateLayer) userInfo:nil repeats:NO];
     
     // Add our view
     [self.view addSubview:self.layerView];
+}
+
+- (void)updateLayer
+{
+    // Force layer to redraw
+    [self.blueLayer display];
+}
+
+- (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)ctx
+{
+    // Draw a thick red circle
+    CGContextSetLineWidth(ctx, 10);
+    CGContextSetStrokeColorWithColor(ctx, [UIColor redColor].CGColor);
+    CGContextStrokeEllipseInRect(ctx, layer.bounds);
 }
 
 
