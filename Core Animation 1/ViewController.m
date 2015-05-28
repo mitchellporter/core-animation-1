6659e24c1aa30f21363848d6737973ff5dcd4088
Mitchell Porter <mitchellporter@gmail.com>
2015-05-28 14:56:59 -0700

    Implemented motion effects

--- Core Animation 1/ViewController.m
+++ Core Animation 1/ViewController.m
@@ -42,10 +42,13 @@
     CATransform3D transform = CATransform3DIdentity;
     
     // Apply perspective
-    transform.m34 = - 1.0 / 500.0;
+//    transform.m34 = - 1.0 / 400.0;
     
     // Rotate by 45 degrees along the Y axis
-    transform = CATransform3DRotate(transform, M_PI_4, 0, 1, 0);
+    transform = CATransform3DScale(transform, 1.5, 1.5, 0);
+    
+    // This visually does the same as scaling really
+//    transform = CATransform3DTranslate(transform, 0, 0, 200);
     
     // Apply to layer
     self.layerView.layer.transform = transform;
