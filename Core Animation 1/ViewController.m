48efd85d061e822910f7dcd3d9bb170b32d1aa98
Mitchell Porter <mitchellporter@gmail.com>
2015-05-28 13:28:29 -0700

    Skipped to chapter 5. Rotating layer by 45 degrees

--- Core Animation 1/ViewController.m
+++ Core Animation 1/ViewController.m
@@ -24,29 +24,21 @@
     // Background color
     self.view.backgroundColor = [UIColor grayColor];
     
-    // Create sublayer
-    self.blueLayer = [CALayer layer];
-    self.blueLayer.frame = CGRectMake(50, 50, 100, 100);
-    self.blueLayer.backgroundColor = [UIColor blueColor].CGColor;
-    
-    // Set controller as layer delegate
-    self.blueLayer.delegate = self;
-    
-    // Ensure that layer backing image uses correct scale
-    self.blueLayer.contentsScale = [UIScreen mainScreen].scale;
-    
-    
     self.layerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
+    self.layerView.backgroundColor = [UIColor blueColor];
     self.layerView.frame = CGRectMake(self.view.frame.size.width/2 - self.layerView.frame.size.width/2, self.view.frame.size.height/2 - self.layerView.frame.size.height, self.layerView.frame.size.width, self.layerView.frame.size.height);
-    
-    // Add layer to our view
-    [self.layerView.layer addSublayer:self.blueLayer];
-    
-    // Redraw layer
-    [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(updateLayer) userInfo:nil repeats:NO];
     
     // Add our view
     [self.view addSubview:self.layerView];
+    
+    [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(transform) userInfo:nil repeats:NO];
+}
+
+- (void)transform
+{
+    // Rotate the layer 45 degrees
+    CGAffineTransform transform = CGAffineTransformMakeRotation(M_PI_4);
+    self.layerView.layer.affineTransform = transform;
 }
 
 - (void)updateLayer
